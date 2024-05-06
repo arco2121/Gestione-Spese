84218729d9000ed4982e9c9a6032a5b87b7e5a23
arco2121 <colombaramarco21@gmail.com>
2024-05-06 22:38:53 +0200

    1

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace GestiFInanza
 {
-    partial class Form1
+    partial class GestiFinanza
     {
         /// <summary>
         /// Variabile di progettazione necessaria.
@@ -28,22 +28,155 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.view = new System.Windows.Forms.DataGridView();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.Inserisci = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.view)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
             this.SuspendLayout();
             // 
-            // Form1
+            // view
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.view.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.view.Location = new System.Drawing.Point(519, 99);
+            this.view.Name = "view";
+            this.view.RowHeadersWidth = 51;
+            this.view.RowTemplate.Height = 24;
+            this.view.Size = new System.Drawing.Size(473, 348);
+            this.view.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.2F);
+            this.label1.Location = new System.Drawing.Point(74, 99);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(89, 30);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Nome:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.2F);
+            this.label2.Location = new System.Drawing.Point(74, 277);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(106, 30);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Importo:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.2F);
+            this.label3.Location = new System.Drawing.Point(74, 189);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(131, 30);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Categoria:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.2F);
+            this.label4.Location = new System.Drawing.Point(74, 365);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(74, 30);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "Data:";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F);
+            this.textBox1.Location = new System.Drawing.Point(228, 104);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(178, 27);
+            this.textBox1.TabIndex = 5;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F);
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(228, 194);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(178, 28);
+            this.comboBox1.TabIndex = 6;
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F);
+            this.numericUpDown1.Location = new System.Drawing.Point(228, 283);
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(178, 27);
+            this.numericUpDown1.TabIndex = 7;
+            this.numericUpDown1.Maximum = new decimal (9999999999999999999);
+            this.numericUpDown1.Minimum = new decimal(1);
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F);
+            this.dateTimePicker1.Location = new System.Drawing.Point(228, 368);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(178, 27);
+            this.dateTimePicker1.TabIndex = 8;
+            // 
+            // Inserisci
+            // 
+            this.Inserisci.Font = new System.Drawing.Font("Microsoft Sans Serif", 12.2F);
+            this.Inserisci.Location = new System.Drawing.Point(163, 439);
+            this.Inserisci.Name = "Inserisci";
+            this.Inserisci.Size = new System.Drawing.Size(148, 34);
+            this.Inserisci.TabIndex = 9;
+            this.Inserisci.Text = "Inserisci";
+            this.Inserisci.UseVisualStyleBackColor = true;
+            this.Inserisci.Click += new System.EventHandler(this.Inserisci_Click);
+            // 
+            // GestiFinanza
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.AutoSize = true;
-            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
-            this.ClientSize = new System.Drawing.Size(1188, 699);
-            this.Name = "Form1";
+            this.ClientSize = new System.Drawing.Size(1080, 588);
+            this.Controls.Add(this.Inserisci);
+            this.Controls.Add(this.dateTimePicker1);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.view);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "GestiFinanza";
             this.Text = "GestiFinanza";
+            this.Load += new System.EventHandler(this.GestiFinanza_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.view)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.DataGridView view;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.Button Inserisci;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -3,18 +3,110 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
+using System.Net.Http.Headers;
+using System.Net.NetworkInformation;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace GestiFInanza
 {
-    public partial class Form1 : Form
+    public partial class GestiFinanza : Form
     {
-        public Form1()
+        public GestiFinanza()
         {
             InitializeComponent();
+        }
+
+        private void GestiFinanza_Load(object sender, EventArgs e)
+        {
+            try
+            {
+                string[] gh = File.ReadAllLines("Config.GestiFinanza");
+                gh = gh.Distinct().ToArray();
+                for (int i = 0; i<gh.Length;i++)
+                {
+                    comboBox1.Items.Add(gh[i]);
+                }
+            }
+            catch { }
+            try { view.DataSource = StampaInTab(); }
+            catch 
+            {
+                string all = "Nome,Categoria,Importo,Data\n";
+                File.WriteAllText("Dati.csv", all);
+                view.DataSource = StampaInTab();
+            }
+        }
+
+        private void Inserisci_Click(object sender, EventArgs e)
+        {
+            string nome = textBox1.Text;
+            string cat = comboBox1.Text;
+            DateTime data = dateTimePicker1.Value;
+            float imp = (float) numericUpDown1.Value;
+            string dta = nome + "," + cat + "," + imp + "," + data + "\n";
+            string j = "";
+            try
+            {
+                j = File.ReadAllText("Config.GestiFinanza");
+                j += cat + "\n";
+                File.WriteAllText("Config.GestiFinanza", j);
+            }
+            catch
+            {
+                j += cat + "\n";
+                File.WriteAllText("Config.GestiFinanza", j);
+            }
+            string h = "";
+            try { h = File.ReadAllText("Dati.csv"); }
+            catch { h = ""; }
+            if (h == "")
+            {
+                string all = "Nome,Categoria,Importo,Data\n" + dta;
+                File.WriteAllText("Dati.csv", all);
+                view.DataSource = StampaInTab();
+            }
+            else
+            {
+                h += dta;
+                File.WriteAllText("Dati.csv", h);
+                view.DataSource = StampaInTab();
+            }
+        }
+
+        private DataTable StampaInTab()
+        {
+            string[] all = File.ReadAllLines("Dati.csv");
+            DataTable tab = new DataTable();
+            for (int i = 0; i<all.Length; i++)
+            {
+                string[] s = all[i].Split(',');
+                if(i == 0)
+                { 
+                    tab.Columns.Add(s[0], typeof(string));
+                    tab.Columns.Add(s[1], typeof(string));
+                    tab.Columns.Add(s[2], typeof(float));
+                    tab.Columns.Add(s[3], typeof(DateTime));
+                }
+                else
+                {
+                    tab.Rows.Add(s[0], s[1], s[2], s[3]);
+                }
+            }
+            try
+            {
+                string[] gh = File.ReadAllLines("Config.GestiFinanza");
+                gh = gh.Distinct().ToArray();
+                for (int i = 0; i < gh.Length; i++)
+                {
+                    comboBox1.Items.Add(gh[i]);
+                }
+            }
+            catch { };
+            return tab;
         }
     }
 }
